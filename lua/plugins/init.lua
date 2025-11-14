2e25bfe0ca724b1f9ba65ea35b5095f568b97a15
dieunn <dieunn@newwave.vn>
2025-11-14 09:15:49 +0700

    Add neogit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -33,4 +33,33 @@ return {
       enable_close_on_slash = false -- Auto close on trailing </
     },
   },
+
+  -- Neogit - Magit-like Git interface for Neovim
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("neogit").setup({
+        integrations = {
+          diffview = true,  -- Enable diffview for side-by-side diffs
+        },
+        commit_editor = {
+          kind = "vsplit",
+          show_staged_diff = true,
+          staged_diff_split_kind = "vsplit",  -- Side-by-side view
+        },
+        commit_view = {
+          kind = "vsplit",
+        },
+      })
+    end,
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
+  },
 }
